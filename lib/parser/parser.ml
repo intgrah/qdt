1f8e525658ad1b007089caae69da95f1d39ba450
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-10-30 09:19:19 +0000

    More

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -1,194 +1,161 @@
-type input = Token.t list
-type pos = int
+module Parser = struct
+  type input = Token.t list
+  type 'a t = input -> ('a * input) option
 
-type 'a parser = input -> pos -> ('a * pos) option
+  let return (x : 'a) : 'a t = fun input -> Some (x, input)
 
-let return (x : 'a) : 'a parser =
-  fun _input pos -> Some (x, pos)
-
-let bind (p : 'a parser) (f : 'a -> 'b parser) : 'b parser =
-  fun input pos ->
-    match p input pos with
+  let bind (p : 'a t) (f : 'a -> 'b t) : 'b t =
+   fun input ->
+    match p input with
     | None -> None
-    | Some (x, pos') -> f x input pos'
+    | Some (x, input') -> f x input'
 
-let (>>=) = bind
-let (let*) = bind
+  let ( >>= ) = bind
+  let ( let* ) = bind
+  let ( >> ) p q = p >>= fun _ -> q
+  let fail : 'a t = fun _input -> None
 
-let (>>) p q = p >>= fun _ -> q
-
-let fail : 'a parser =
-  fun _input _pos -> None
-
-let alt (p1 : 'a parser) (p2 : 'a parser) : 'a parser =
-  fun input pos ->
-    match p1 input pos with
+  let alt (p1 : 'a t) (p2 : 'a t) : 'a t =
+   fun input ->
+    match p1 input with
     | Some _ as result -> result
-    | None -> p2 input pos
+    | None -> p2 input
 
-let (<|>) = alt
+  let ( <|> ) = alt
 
-let peek : Token.t option parser =
-  fun input pos ->
-    if pos >= List.length input then
-      Some (None, pos)
-    else
-      Some (Some (List.nth input pos), pos)
+  let peek : Token.t option t =
+   fun input ->
+    match input with
+    | [] -> Some (None, input)
+    | tok :: _ -> Some (Some tok, input)
 
-let satisfy (pred : Token.t -> bool) : Token.t parser =
-  fun input pos ->
-    if pos >= List.length input then
-      None
-    else if pred (List.nth input pos) then
-      Some (List.nth input pos, pos + 1)
-    else
-      None
+  let satisfy (pred : Token.t -> bool) : Token.t t =
+   fun input ->
+    match input with
+    | [] -> None
+    | tok :: rest when pred tok -> Some (tok, rest)
+    | _ -> None
 
-let token (t : Token.t) : unit parser =
-  let* _ = satisfy (fun tok -> tok = t) in
-  return ()
+  let token (t : Token.t) : unit t =
+    let* _ = satisfy (fun tok -> tok = t) in
+    return ()
 
-let many (p : 'a parser) : 'a list parser =
-  let rec go acc input pos =
-    match p input pos with
-    | None -> Some (List.rev acc, pos)
-    | Some (x, pos') -> go (x :: acc) input pos'
-  in
-  fun input pos -> go [] input pos
+  let many (p : 'a t) : 'a list t =
+    let rec go acc input =
+      match p input with
+      | None -> Some (List.rev acc, input)
+      | Some (x, input') -> go (x :: acc) input'
+    in
+    fun input -> go [] input
 
-let some (p : 'a parser) : 'a list parser =
+  let some (p : 'a t) : 'a list t =
+    let* x = p in
+    let* xs = many p in
+    return (x :: xs)
+end
+
+open Parser
+
+let parse_ident : string t =
+ fun input ->
+  match input with
+  | Token.Ident name :: rest -> Some (name, rest)
+  | _ -> None
+
+let parse_parens (p : 'a t) : 'a t =
+  let* () = token Token.LParen in
   let* x = p in
-  let* xs = many p in
-  return (x :: xs)
-
-let parse_ident : string parser =
-  fun input pos ->
-    if pos >= List.length input then
-      None
-    else
-      match List.nth input pos with
-      | Token.Ident name -> Some (name, pos + 1)
-      | _ -> None
-
-let parse_lparen : unit parser = token Token.LParen
-let parse_rparen : unit parser = token Token.RParen
-let parse_colon : unit parser = token Token.Colon
-let parse_arrow : unit parser = token Token.Arrow
-let parse_colon_equals : unit parser = token Token.Colon_equals
-let parse_let : unit parser = token Token.Let
-let parse_fun : unit parser = token Token.Fun
-
-let parse_parens (p : 'a parser) : 'a parser =
-  let* _ = parse_lparen in
-  let* x = p in
-  let* _ = parse_rparen in
+  let* () = token Token.RParen in
   return x
 
-let rec parse_tm_atom : Cst.tm parser =
-  fun input pos ->
-    (parse_var
-     <|> parse_hole
-     <|> parse_parens parse_tm) input pos
+let rec parse_tm_atom : Cst.tm t =
+ fun input -> (parse_var <|> parse_hole <|> parse_parens parse_tm) input
 
-and parse_var : Cst.tm parser =
-  fun input pos ->
-    (parse_ident >>= fun name ->
-     return (Cst.Var name)) input pos
+and parse_var : Cst.tm t =
+ fun input ->
+  (let* name = parse_ident in
+   return (Cst.Var name))
+    input
 
-and parse_hole : Cst.tm parser =
-  fun input pos ->
-    if pos >= List.length input then
-      None
-    else
-      match List.nth input pos with
-      | Token.Ident "_" -> Some (Cst.Hole, pos + 1)
-      | _ -> None
+and parse_hole : Cst.tm t = function
+  | Token.Underscore :: rest -> Some (Cst.Hole, rest)
+  | _ -> None
 
-and parse_lambda : Cst.tm parser =
-  fun input pos ->
-    (parse_fun >>= fun _ ->
-     parse_ident >>= fun name ->
-     ((parse_colon >>= fun _ ->
-       parse_ty >>= fun ty ->
-       return (Some ty))
-      <|> return None) >>= fun ty_opt ->
-     parse_arrow >>= fun _ ->
-     parse_tm >>= fun body ->
-     return (Cst.Lambda (name, ty_opt, body))) input pos
+and parse_lambda : Cst.tm t =
+ fun input ->
+  (let* _ = token Token.Fun in
+   let* name = parse_ident in
+   let* ty_opt =
+     (let* _ = token Token.Colon in
+      let* ty = parse_ty in
+      return (Some ty))
+     <|> return None
+   in
+   let* _ = token Token.Arrow in
+   let* body = parse_tm in
+   return (Cst.Lambda (name, ty_opt, body)))
+    input
 
-and parse_ann : Cst.tm parser =
-  fun input pos ->
-    (parse_tm_app >>= fun tm ->
-     ((parse_colon >>= fun _ ->
-       parse_ty >>= fun ty ->
-       return (Some ty))
-      <|> return None) >>= fun ty_opt ->
-     match ty_opt with
-     | Some ty -> return (Cst.Ann (tm, ty))
-     | None -> return tm) input pos
+and parse_ann : Cst.tm t =
+ fun input ->
+  (let* tm = parse_tm_app in
+   let* ty_opt =
+     (let* _ = token Token.Colon in
+      let* ty = parse_ty in
+      return (Some ty))
+     <|> return None
+   in
+   match ty_opt with
+   | Some ty -> return (Cst.Ann (tm, ty))
+   | None -> return tm)
+    input
 
-and parse_tm_app : Cst.tm parser =
-  fun input pos ->
-    (parse_tm_atom >>= fun head ->
-     many parse_tm_atom >>= fun args ->
-     return (List.fold_left (fun f a -> Cst.App (f, a)) head args)) input pos
+and parse_tm_app : Cst.tm t =
+ fun input ->
+  (let* head = parse_tm_atom in
+   let* args = many parse_tm_atom in
+   return (List.fold_left (fun f a -> Cst.App (f, a)) head args))
+    input
 
-and parse_tm : Cst.tm parser =
-  fun input pos ->
-    (parse_lambda <|> parse_ann) input pos
+and parse_tm : Cst.tm t = fun input -> (parse_lambda <|> parse_ann) input
 
-and parse_ty_atom : Cst.ty parser =
-  fun input pos ->
-    (parse_star
-     <|> parse_ty_var
-     <|> parse_parens parse_ty) input pos
+and parse_ty_atom : Cst.ty t =
+ fun input -> (parse_star <|> parse_ty_var <|> parse_parens parse_ty) input
 
-and parse_star : Cst.ty parser =
-  fun input pos ->
-    if pos >= List.length input then
-      None
-    else
-      match List.nth input pos with
-      | Token.Ident "Type" -> Some (Cst.Star, pos + 1)
-      | _ -> None
+and parse_star : Cst.ty t = function
+  | Token.Ident "Type" :: rest -> Some (Cst.Star, rest)
+  | _ -> None
 
-and parse_ty_var : Cst.ty parser =
-  fun input pos ->
-    (parse_ident >>= fun name ->
-     return (Cst.TVar name)) input pos
+and parse_ty_var : Cst.ty t =
+ fun input ->
+  (let* name = parse_ident in
+   return (Cst.TVar name))
+    input
 
-and parse_pi : Cst.ty parser =
-  fun input pos ->
-    if pos >= List.length input then
-      None
-    else
-      match List.nth input pos with
-      | Token.Ident "forall" ->
-          (return () >>= fun _ ->
-           parse_ident >>= fun name ->
-           parse_colon >>= fun _ ->
-           parse_ty >>= fun a ->
-           token (Token.Ident ".") >>= fun _ ->
-           parse_ty >>= fun b ->
-           return (Cst.Pi (name, a, b))) input (pos + 1)
-      | _ -> None
+and parse_pi : Cst.ty t = function
+  | Token.Ident "forall" :: rest ->
+      (let* name = parse_ident in
+       let* _ = token Token.Colon in
+       let* a = parse_ty in
+       let* _ = token (Token.Ident ".") in
+       let* b = parse_ty in
+       return (Cst.Pi (name, a, b)))
+        rest
+  | _ -> None
 
-and parse_arrow_ty : Cst.ty parser =
-  fun input pos ->
-    (parse_ty_atom >>= fun a ->
-     parse_arrow >>= fun _ ->
-     parse_ty >>= fun b ->
-     return (Cst.Arrow (a, b))) input pos
+and parse_arrow_ty : Cst.ty t =
+ fun input ->
+  (let* a = parse_ty_atom in
+   let* _ = token Token.Arrow in
+   let* b = parse_ty in
+   return (Cst.Arrow (a, b)))
+    input
 
-and parse_ty : Cst.ty parser =
-  fun input pos ->
-    (parse_pi <|> parse_arrow_ty <|> parse_ty_atom) input pos
+and parse_ty : Cst.ty t =
+ fun input -> (parse_pi <|> parse_arrow_ty <|> parse_ty_atom) input
 
-let parse (p : 'a parser) (input : Token.t list) : ('a, string) result =
-  match p input 0 with
-  | None -> Error "parse error"
-  | Some (x, pos) ->
-      if pos = List.length input then
-        Ok x
-      else
-        Error ("unexpected token at position " ^ string_of_int pos)
+let parse (p : 'a t) (input : Token.t list) : ('a, _) result =
+  match p input with
+  | None -> Error `Parse_error
+  | Some (x, []) -> Ok x
+  | Some (_, _ :: _) -> Error `Tokens_remaining
