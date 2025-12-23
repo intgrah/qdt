7ed7a7c9aa8617d353a7d5dd035400380d7d6430
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-12-23 22:19:39 +0000

    Split desugar.ml

--- frontend/parser.mli
+++ frontend/parser.mli
@@ -1,4 +1,4 @@
 exception Parse_error of string
 exception Tokens_remaining of Lexer.token list
 
-val parse : Lexer.token list -> Elaboration.Syntax.Raw.program
+val parse : Lexer.token list -> Raw_syntax.program
