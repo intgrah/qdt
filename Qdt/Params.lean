2eff75229f43f9d4ebcf018c41d9192f664d23f6
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-02-02 03:21:52 +0000

    Simplify Params a bit

--- Qdt/Params.lean
+++ Qdt/Params.lean
@@ -8,11 +8,11 @@ open Lean (Name)
 
 /-- Elaborate parameters and return their types with universe levels -/
 def elabParamsWithLevels {n} (params : List Frontend.Ast.TypedBinder) :
-    TermM n (TermContext (n + params.length) × Tele Param n (n + params.length) × List Universe) := by
-  simpa only [Nat.zero_add] using go n .nil [] params
+    TermM n (TermContext (n + params.length) × Tele Param n (n + params.length) × List Universe) :=
+  Nat.zero_add params.length ▸  go n .nil [] params
   where
     go {a : Nat} (b : Nat) (acc : Tele Param a b) (levels : List Universe) :
-        (params : List Frontend.Ast.TypedBinder) →
+      (params : List Frontend.Ast.TypedBinder) →
         TermM b (TermContext (b + params.length) × Tele Param a (b + params.length) × List Universe)
     | [] => return (← read, acc, levels.reverse)
     | ⟨src, name, ty⟩ :: bs => do
@@ -31,17 +31,14 @@ def elabParamsFrom {n} (params : List Frontend.Ast.TypedBinder) :
 
 
 def elabParams (params : List Frontend.Ast.TypedBinder) :
-    MetaM (TermContext params.length × Tele Param 0 params.length) := by
-  simpa only [Nat.zero_add] using elabParamsFrom params TermContext.empty
+    MetaM (TermContext params.length × Tele Param 0 params.length) :=
+  Nat.zero_add params.length ▸ elabParamsFrom params TermContext.empty
 
 def elabVParamsFrom {n} (params : List Frontend.Ast.TypedBinder) :
-    TermM n (TermContext (n + params.length) × Tele VParam n (n + params.length)) := by
-  simpa only [Nat.zero_add] using elabVParamsFrom.go n .nil params
+    TermM n (TermContext (n + params.length) × Tele VParam n (n + params.length)) :=
+  Nat.zero_add params.length ▸ go n .nil params
   where
-    elabVParamsFrom.go
-      {a : Nat}
-      (b : Nat)
-      (acc : Tele VParam a b) :
+    go {a : Nat} (b : Nat) (acc : Tele VParam a b) :
       (params : List Frontend.Ast.TypedBinder) →
       TermM b (TermContext (b + params.length) × Tele VParam a (b + params.length))
     | [] => return (← read, acc)
@@ -52,7 +49,7 @@ def elabVParamsFrom {n} (params : List Frontend.Ast.TypedBinder) :
         let ctx := ctx.bind name tyVal
         return by
           simpa only [List.length_cons, Nat.add_comm bs.length, ← Nat.add_assoc b]
-          using ← elabVParamsFrom.go (b + 1) (acc.snoc ⟨name, tyVal⟩) bs ctx
+          using ← go (b + 1) (acc.snoc ⟨name, tyVal⟩) bs ctx
 
 
 def elabVParams (params : List Frontend.Ast.TypedBinder) :
