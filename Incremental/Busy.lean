cc4df0319c04e0616bd24a98098a17684000c507
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-07 23:11:24 +0000

    busy

--- Incremental/Busy.lean
+++ Incremental/Busy.lean
@@ -10,15 +10,10 @@ namespace Busy
 
 open Std (DHashMap)
 
-variable (Q : Type) (R : Q → Type)
-  [BEq Q] [LawfulBEq Q] [Hashable Q]
-
-def Store := DHashMap Q R
-
 variable {Q : Type} {R : Q → Type}
   [BEq Q] [LawfulBEq Q] [Hashable Q]
 
-partial def build : Build Applicative (Store Q R) Q R :=
+partial def build : Build Applicative (DHashMap Q R) Q R :=
   fun tasks target store => runEST fun σ => do
     let stRef ← ST.mkRef (σ := σ) store
     let rec fetch (q : Q) : EST BuildError σ (R q) := do
