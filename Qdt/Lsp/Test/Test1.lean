5e9546a6744eed5f19ff83f6d81b8388d8cbecbc
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-08 19:11:29 +0000

    Dedent

--- Qdt/Lsp/Test/Test1.lean
+++ Qdt/Lsp/Test/Test1.lean
@@ -1,21 +1,23 @@
 import Qdt.Lsp.Test
 
 open Qdt.Lsp.Test
-open Qdt
 
 #eval test do
-  setText "test.qdt" qdt!(
-    def foo := Type
-    def bar := foo
-  )
-  diagnostics "test.qdt" Array.isEmpty
-  hover "test.qdt" ⟨1, 4⟩ "foo : Type 1"
-  hover "test.qdt" ⟨2, 8⟩ "bar : Type 1"
 
-  setText "test.qdt" qdt!(
-    def bar := foo
-    def foo := Type
-  )
-  diagnostics "test.qdt" (· matches #[⟨_, .unboundVariable ..⟩])
-  noHover "test.qdt" ⟨1, 4⟩
-  hover "test.qdt" ⟨2, 8⟩ "foo : Type 1"
+setText qdt!(
+def foo := Type
+def bar := foo
+)
+
+noDiagnostics
+hover ⟨1, 4⟩ "foo : Type 1"
+hover ⟨2, 4⟩ "bar : Type 1"
+
+setText qdt!(
+def bar := foo
+def foo := Type
+)
+
+diagnostics (· matches #[⟨_, .unboundVariable ..⟩])
+noHover ⟨1, 4⟩
+hover ⟨2, 4⟩ "foo : Type 1"
