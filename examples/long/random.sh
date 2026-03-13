ef1cefe9ee310d4fde9a0f72d298406ae76a558e
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-13 03:53:55 +0000

    examples

--- examples/long/random.sh
+++ examples/long/random.sh
@@ -4,8 +4,8 @@ echo "inductive Nat where"
 echo "  | zero"
 echo "  | succ (n : Nat) : Nat"
 echo
-echo "inductive Eq.{u} (A : Type u) (a : A) : A → Type u where"
-echo "  | refl : Eq.{u} A a a"
+echo "inductive Eq.{u} (α : Type u) (a : α) : α → Type u where"
+echo "  | refl : Eq.{u} α a a"
 echo
 echo "def n0 : Nat := Nat.zero"
 for ((i = 1; i <= ${N:-5000}; i++)); do
