bede395dcbd78fa01c422f878c3ea65e5a40d4ab
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-16 13:44:13 +0000

    Triangle

---
+++ examples/long/triangle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "inductive Nat where"
+echo "  | zero"
+echo "  | succ (n : Nat) : Nat"
+echo
+echo "def Nat.add (m : Nat) : Nat → Nat :="
+echo "  Nat.rec.{0} (fun (_ : Nat) => Nat) m (fun (_ : Nat) => Nat.succ)"
+echo
+echo "def n0 : Nat := Nat.zero"
+for ((i = 1; i <= ${N:-10}; i++)); do
+    printf "def n$i : Nat := "
+    for ((j = 0; j < i; j++)); do
+        if ((j > 0)); then
+            printf " + "
+        fi
+        printf "n$j"
+    done
+    echo
+done
