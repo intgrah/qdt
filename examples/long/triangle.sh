0affcd3a15c30aa283e768f1139502cade25c3ef
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-18 03:52:40 +0000

    Increase triangle bench

--- examples/long/triangle.sh
+++ examples/long/triangle.sh
@@ -8,7 +8,7 @@ echo "def Nat.add (m : Nat) : Nat → Nat :="
 echo "  Nat.rec.{0} (fun (_ : Nat) => Nat) m (fun (_ : Nat) => Nat.succ)"
 echo
 echo "def n0 : Nat := Nat.zero"
-for ((i = 1; i <= ${N:-10}; i++)); do
+for ((i = 1; i <= ${N:-100}; i++)); do
     printf "def n$i : Nat := "
     for ((j = 0; j < i; j++)); do
         if ((j > 0)); then
