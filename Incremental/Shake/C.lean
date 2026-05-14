815aa8d9e3a12df27e278c9c8d2db45645be7cb0
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-05-14 17:56:50 +0100

    it's inhabited

--- Incremental/Shake/C.lean
+++ Incremental/Shake/C.lean
@@ -12,8 +12,8 @@ variable
   [BEq ℭ.I] [LawfulBEq ℭ.I] [Hashable ℭ.I] [∀ i, Hashable (ℭ.V i)]
   [BEq ℭ.Q] [LawfulBEq ℭ.Q] [Hashable ℭ.Q] [∀ q, Hashable (ℭ.R q)]
 
-instance : Inhabited (Tasks ℭ → ∀ q, ShakeRT.Store ℭ J → ℭ.R q × ShakeRT.Store ℭ J) :=
-  ⟨sorry⟩
+instance : Inhabited (Tasks ℭ → ∀ q, ShakeRT.Store ℭ J → ℭ.R q × ShakeRT.Store ℭ J) where
+  default tasks q s := ⟨compute tasks (Input.get s.inputs) q, s⟩
 
 @[extern "lean_shake_build"]
 public opaque shakeCBuild
@@ -25,7 +25,6 @@ public opaque shakeCBuild
     ShakeRT.Store ℭ J → ℭ.R q × ShakeRT.Store ℭ J
 
 @[expose] public def ShakeC (tasks : Tasks ℭ) : Build ℭ J tasks Id Id where
-
   σ := ShakeRT.Store ℭ J
   init inputs := {
     inputs
