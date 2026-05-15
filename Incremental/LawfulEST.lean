b9ca259a103303a0a4eced54b21b9ea7424f6cef
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-05-15 22:43:03 +0100

    "lawful" ST and EST

--- Incremental/LawfulEST.lean
+++ Incremental/LawfulEST.lean
@@ -11,30 +11,23 @@ namespace Incremental
 variable {ε σ : Type}
 
 /-!
-We use `EST` but we can't actually prove anything about it.
-Just assume it's well behaved.
+Just assume `ST` and `EST` are well behaved...
 
-If you define a build system that, for example, checks a boolean "cancelled?" flag in another thread,
-you need `IO`. Same thing if you need to benchmark queries, with `IO.monoNanosNow` for example.
-We aren't relying on these for soundness anyway, if you really want verifiability, just don't use those build systems.
+See LawfulEst_.lean
 -/
-
-axiom EST.instLawfulMonad : LawfulMonad (EST ε σ)
-attribute [instance] EST.instLawfulMonad
-
-axiom EST.lawfulMonadAttach : LawfulMonadAttach (EST ε σ)
-attribute [instance] EST.lawfulMonadAttach
-
-instance : LawfulMonad (EIO ε) := inferInstanceAs (LawfulMonad (EST ε IO.RealWorld))
-instance : LawfulMonadAttach (EIO ε) := inferInstanceAs (LawfulMonadAttach (EST ε IO.RealWorld))
 
 axiom ST.instLawfulMonad : LawfulMonad (ST σ)
 attribute [instance] ST.instLawfulMonad
-
 axiom ST.lawfulMonadAttach : LawfulMonadAttach (ST σ)
 attribute [instance] ST.lawfulMonadAttach
+axiom EST.instLawfulMonad : LawfulMonad (EST ε σ)
+attribute [instance] EST.instLawfulMonad
+axiom EST.lawfulMonadAttach : LawfulMonadAttach (EST ε σ)
+attribute [instance] EST.lawfulMonadAttach
 
 instance : LawfulMonad BaseIO := inferInstanceAs (LawfulMonad (ST IO.RealWorld))
 instance : LawfulMonadAttach BaseIO := inferInstanceAs (LawfulMonadAttach (ST IO.RealWorld))
+instance : LawfulMonad (EIO ε) := inferInstanceAs (LawfulMonad (EST ε IO.RealWorld))
+instance : LawfulMonadAttach (EIO ε) := inferInstanceAs (LawfulMonadAttach (EST ε IO.RealWorld))
 
 end Incremental
