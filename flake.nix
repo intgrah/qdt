1783cba5b962f3de02865ce70e18b7446c2cabf6
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-05-23 17:50:37 +0100

    .

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            just
+            jq
+            elan
+            fish
+            bun
+            tree-sitter
+            rustup
+          ];
+        };
+      }
+    );
+}
